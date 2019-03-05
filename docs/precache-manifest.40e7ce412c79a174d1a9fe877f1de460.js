12ff2d34d492db6ec651e9027cdfcc06c6cc7ca7
Alex <alex.alexeev23061991@gmail.com>
2019-03-05 16:43:48 +0300

    done

---
+++ docs/precache-manifest.40e7ce412c79a174d1a9fe877f1de460.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "341872a27360bbed55df",
+    "url": "/static/css/main.75d9ce87.chunk.css"
+  },
+  {
+    "revision": "341872a27360bbed55df",
+    "url": "/static/js/main.341872a2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "33a37e36d6f4f8e590af",
+    "url": "/static/js/2.33a37e36.chunk.js"
+  },
+  {
+    "revision": "45ca4379f69cd404f5e00fc1c1df2862",
+    "url": "/static/media/cart.45ca4379.png"
+  },
+  {
+    "revision": "9eddd6f1526d1e3b11a6c9a77a29b912",
+    "url": "/static/media/hero.9eddd6f1.png"
+  },
+  {
+    "revision": "133d62f4c7611596b265b81bfb9be08c",
+    "url": "/static/media/404.133d62f4.gif"
+  },
+  {
+    "revision": "1020953e846909ef2e3d9c52b973613a",
+    "url": "/index.html"
+  }
+];
